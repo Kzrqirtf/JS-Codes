3f2fcdeae76af578ecdeb9cb3660d73a88d14600
Kzrqirtf <128362461+Kzrqirtf@users.noreply.github.com>
2024-01-17 09:15:36 +0000

    commit

---
+++ Basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 144553 //constant cariable,cannot change the value
+let accountEmail = "kritika2004@gmail.com"
+var accountPAssword = "123445"  //do not use war now
+accountCity = "Dehradun"   //can do this, but avoid it
+console.log(accountEmail);
+let state //undefined
+console.table([accountEmail,accountID,accountPAssword,accountCity,state])
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let name="kritika"
+let isloggedin = false
+let age = 10
+
+/*
+Data types in JAVASCRIPT
+primitive:
+number - 2 to power 53
+bigint
+string =""
+boolean = true/false
+null = standalone value
+undefined
+symbol = unique
+*/
+//object
+console.log(typeof null);//object
+console.log(typeof undefined)
+//undefined
+
